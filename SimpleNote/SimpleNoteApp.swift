394380bb9cc2872bc1c3e6efdb625cdcdaedc3b5
parsasd <79263602+parsasd@users.noreply.github.com>
2025-08-24 15:41:07 +0330

    Initial Commit

---
+++ SimpleNote/SimpleNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNoteApp.swift
+//  SimpleNote
+//
+//  Created by parsa sedighi on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleNoteTests/SimpleNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNoteTests.swift
+//  SimpleNoteTests
+//
+//  Created by parsa sedighi on 8/24/25.
+//
+
+import Testing
+@testable import SimpleNote
+
+struct SimpleNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
